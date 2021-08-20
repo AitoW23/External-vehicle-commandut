ec5c80ad3ee5957989beca17f0e9541f81a6a63f
AitoW23 <81420883+AitoW23@users.noreply.github.com>
2021-08-20 11:03:11 +0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'AitoW/AitoW#4424'
+description 'Pystyt avaamaan ovet sisältä autosta ja ulkoa! Jos tulee ongelmia Lähetä discordiiin viesti AitoW#4424'
+
+client_script 'client.lua'
